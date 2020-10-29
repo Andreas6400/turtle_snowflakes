03953d593d7bca84a04feaac17be6a6413efdfda
Andreas6400 <58548723+Andreas6400@users.noreply.github.com>
2020-10-29 15:29:38 +0100

    Update turtle snowflakes.py
    2 commit

--- turtle snowflakes.py
+++ turtle snowflakes.py
@@ -6,26 +6,25 @@ import random
 elsa = turtle.Turtle()
 turtle.Screen().bgcolor("grey")
 colours = ["cyan", "red", "green","brown"]
-
+#anfangs strich der flocke
 elsa.penup()
 elsa.forward(90)
 elsa.left(45)
 elsa.pendown()
-
+# definition der schneeflocke
 def branch():
     for i in range(3):
         for i in range(3):
             elsa.forward(30)
             elsa.backward(30)
             elsa.right(45)
-        elsa.left(90)
+        elsa.right(90)
         elsa.backward(30)
         elsa.left(45)
     elsa.right(90)
     elsa.forward(90)
-    
+# bau der schnneflocke
 for i in range(8):
     branch()
     elsa.left(45)
     elsa.color(random.choice(colours))
-#elsa.color(random.choice(colours))
