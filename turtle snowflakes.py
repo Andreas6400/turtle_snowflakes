19a540afaafab3e34ecd9d0cf72264e9ca196577
Andreas6400 <58548723+Andreas6400@users.noreply.github.com>
2020-10-29 15:14:54 +0100

    Create turtle snowflakes.py
    First comit

---
+++ turtle snowflakes.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+# impotieren von Funktionen
+import turtle
+import random
+# Turtle erstellen, Hindergrundfarben definieren
+elsa = turtle.Turtle()
+turtle.Screen().bgcolor("grey")
+colours = ["cyan", "red", "green","brown"]
+
+elsa.penup()
+elsa.forward(90)
+elsa.left(45)
+elsa.pendown()
+
+def branch():
+    for i in range(3):
+        for i in range(3):
+            elsa.forward(30)
+            elsa.backward(30)
+            elsa.right(45)
+        elsa.left(90)
+        elsa.backward(30)
+        elsa.left(45)
+    elsa.right(90)
+    elsa.forward(90)
+    
+for i in range(8):
+    branch()
+    elsa.left(45)
+    elsa.color(random.choice(colours))
+#elsa.color(random.choice(colours))
